7a3162003f019096689f99b303ac091934731321
MarcusTollefsen <113443191+MarcusTollefsen@users.noreply.github.com>
2024-04-24 14:04:24 +0200

    Update Teams.jsx

---
+++ Pokeapp/src/components/Teams.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const teams = [
+  { id: 1, name: 'Team Aqua', slug: 'team-aqua' },
+  { id: 2, name: 'Team Magma', slug: 'team-magma' },
+  { id: 3, name: 'Team Rocket', slug: 'team-rocket' }
+];
+
+function Teams() {
+  return (
+    <div>
+      <h1>Teamsss</h1>
+      {teams.map(team => (
+        <div key={team.id}>
+          <h2>{team.name}</h2>
+          <Link to={`/teams/${team.slug}`}>View Team</Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Teams; 
