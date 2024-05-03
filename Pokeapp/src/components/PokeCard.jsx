c73ee7d09266f76433a1b20e376e2b2f220e8980
Amkoding <mohamewa@hiof.no>
2024-05-03 19:06:29 +0200

    last update

--- Pokeapp/src/components/PokeCard.jsx
+++ Pokeapp/src/components/PokeCard.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 function PokeCard({ pokemon }) {
-<<<<<<< HEAD
+
   return (
     <figure className="pokemon-card">
       <Link to={`/pokemons/${pokemon.name}`}>
@@ -10,20 +10,7 @@ function PokeCard({ pokemon }) {
       </Link>
     </figure>
   );
-=======
-    return (
-        <article className="pokemon-card">
-            <Link to={`/pokemons/${pokemon.name}`}>
-                <section className="pokemon-image-card">
-                    <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-                </section>
-                <section className="pokemon-info">
-                    <h4>{pokemon.name.toUpperCase()}</h4>
-                </section>
-            </Link>
-        </article>
-    );
->>>>>>> 0fa7903b6f82bd904e2ec2a40e2639ccd1be95bf
+
 }
 
 export default PokeCard;
