0fa7903b6f82bd904e2ec2a40e2639ccd1be95bf
MarcusTollefsen <113443191+MarcusTollefsen@users.noreply.github.com>
2024-05-03 17:57:30 +0200

    div kvote......

--- Pokeapp/src/components/PokeCard.jsx
+++ Pokeapp/src/components/PokeCard.jsx
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom';
 function PokeCard({ pokemon }) {
     return (
-        <div className="pokemon-card">
+        <article className="pokemon-card">
             <Link to={`/pokemons/${pokemon.name}`}>
-                <div className="pokemon-image-card">
+                <section className="pokemon-image-card">
                     <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-                </div>
-                <div className="pokemon-info">
+                </section>
+                <section className="pokemon-info">
                     <h4>{pokemon.name.toUpperCase()}</h4>
-                </div>
+                </section>
             </Link>
-        </div>
+        </article>
     );
 }
 
